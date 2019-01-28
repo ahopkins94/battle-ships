b1dc3304d429d9fb18dce1ca8e45440497957d17
Anouska Hopkins <anouskahopkins@gmail.com>
2019-01-28 18:00:11 +0000

    Amendments made so boat location form renders first

--- src/components/game.js
+++ src/components/game.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import { PlayerBoard } from './PlayerBoard'
+import { BoatLocationForm } from './boatLocationForm'
 
 export class Game extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentBoat: 7
-    }
-  }
 
   render() {
     return(
       <div id="Game">
-      <div>Please enter your boats' coordinates below:</div>
       <div>
-        <PlayerBoard />
+        <BoatLocationForm />
       </div>
       </div>
     )
