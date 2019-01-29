521ab7304bb4b3636b94f8c344c34fb1d12e8f30
Anouska Hopkins <anouskahopkins@gmail.com>
2019-01-29 12:29:44 +0000

    Looks to app component not game component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Game } from './components/game';
+import { App } from './components/app';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render((<BrowserRouter>
+                  <App />
+                </BrowserRouter>), document.getElementById('root'));
