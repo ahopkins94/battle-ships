18e479d10e09072ae54db73f213ebae70ef1935d
Anouska Hopkins <anouskahopkins@gmail.com>
2019-02-11 15:13:11 +0000

    Refactored, less repetition

--- src/components/boatLocationForm.js
+++ src/components/boatLocationForm.js
@@ -28,143 +28,25 @@ export class BoatLocationForm extends React.Component {
       battleshipLocation6: null,
       battleshipLocation7: null,
       battleshipLocation8: null,
-      allPlayerLocations: Array(60).fill(null),
+      allPlayerLocations: Array(61).fill(null),
       submitted: false
     }
 
   }
 
   handleChange(event) {
-      if (event.target.name === "dinghyLocation1") {
-        let clonedDinghyLocation1 = this.state.dinghyLocation1
-        clonedDinghyLocation1 = event.target.value
-        this.setState({dinghyLocation1: clonedDinghyLocation1})
-      }
-      if (event.target.name === "dinghyLocation2") {
-        let clonedDinghyLocation2 = this.state.dinghyLocation2
-        clonedDinghyLocation2 = event.target.value
-        this.setState({dinghyLocation2: clonedDinghyLocation2})
-      }
-      if (event.target.name === "dinghyLocation3") {
-        let clonedDinghyLocation3 = this.state.dinghyLocation3
-        clonedDinghyLocation3 = event.target.value
-        this.setState({dinghyLocation3: clonedDinghyLocation3})
-      }
-      if (event.target.name === "submarineLocation1") {
-        let clonedSubmarineLocation1 = this.state.submarineLocation1
-        clonedSubmarineLocation1 = event.target.value
-        this.setState({submarineLocation1: clonedSubmarineLocation1})
-      }
-      if (event.target.name === "submarineLocation2") {
-        let clonedSubmarineLocation2 = this.state.submarineLocation2
-        clonedSubmarineLocation2 = event.target.value
-        this.setState({submarineLocation2: clonedSubmarineLocation2})
-      }
-      if (event.target.name === "submarineLocation3") {
-        let clonedSubmarineLocation3 = this.state.submarineLocation3
-        clonedSubmarineLocation3 = event.target.value
-        this.setState({submarineLocation3: clonedSubmarineLocation3})
-      }
-      if (event.target.name === "submarineLocation4") {
-        let clonedSubmarineLocation4 = this.state.submarineLocation4
-        clonedSubmarineLocation4 = event.target.value
-        this.setState({submarineLocation4: clonedSubmarineLocation4})
-      }
-      if (event.target.name === "destroyerLocation1") {
-        let clonedDestroyerLocation1 = this.state.destroyerLocation1
-        clonedDestroyerLocation1 = event.target.value
-        this.setState({destroyerLocation1: clonedDestroyerLocation1})
-      }
-      if (event.target.name === "destroyerLocation2") {
-        let clonedDestroyerLocation2 = this.state.destroyerLocation2
-        clonedDestroyerLocation2 = event.target.value
-        this.setState({destroyerLocation2: clonedDestroyerLocation2})
-      }
-      if (event.target.name === "destroyerLocation3") {
-        let clonedDestroyerLocation3 = this.state.destroyerLocation3
-        clonedDestroyerLocation3 = event.target.value
-        this.setState({destroyerLocation3: clonedDestroyerLocation3})
-      }
-      if (event.target.name === "destroyerLocation4") {
-        let clonedDestroyerLocation4 = this.state.destroyerLocation4
-        clonedDestroyerLocation4 = event.target.value
-        this.setState({destroyerLocation4: clonedDestroyerLocation4})
-      }
-      if (event.target.name === "destroyerLocation5") {
-        let clonedDestroyerLocation5 = this.state.destroyerLocation5
-        clonedDestroyerLocation5 = event.target.value
-        this.setState({destroyerLocation5: clonedDestroyerLocation5})
-      }
-      if (event.target.name === "destroyerLocation6") {
-        let clonedDestroyerLocation6 = this.state.destroyerLocation6
-        clonedDestroyerLocation6 = event.target.value
-        this.setState({destroyerLocation6: clonedDestroyerLocation6})
-      }
-      if (event.target.name === "battleshipLocation1") {
-        let clonedBattleshipLocation1 = this.state.battleshipLocation1
-        clonedBattleshipLocation1 = event.target.value
-        this.setState({battleshipLocation1: clonedBattleshipLocation1})
-      }
-      if (event.target.name === "battleshipLocation2") {
-        let clonedBattleshipLocation2 = this.state.battleshipLocation2
-        clonedBattleshipLocation2 = event.target.value
-        this.setState({battleshipLocation2: clonedBattleshipLocation2})
-      }
-      if (event.target.name === "battleshipLocation3") {
-        let clonedBattleshipLocation3 = this.state.battleshipLocation3
-        clonedBattleshipLocation3 = event.target.value
-        this.setState({battleshipLocation3: clonedBattleshipLocation3})
-      }
-      if (event.target.name === "battleshipLocation4") {
-        let clonedBattleshipLocation4 = this.state.battleshipLocation4
-        clonedBattleshipLocation4 = event.target.value
-        this.setState({battleshipLocation4: clonedBattleshipLocation4})
-      }
-      if (event.target.name === "battleshipLocation5") {
-        let clonedBattleshipLocation5 = this.state.battleshipLocation5
-        clonedBattleshipLocation5 = event.target.value
-        this.setState({battleshipLocation5: clonedBattleshipLocation5})
-      }
-      if (event.target.name === "battleshipLocation6") {
-        let clonedBattleshipLocation6 = this.state.battleshipLocation6
-        clonedBattleshipLocation6 = event.target.value
-        this.setState({battleshipLocation6: clonedBattleshipLocation6})
-      }
-      if (event.target.name === "battleshipLocation7") {
-        let clonedBattleshipLocation7 = this.state.battleshipLocation7
-        clonedBattleshipLocation7 = event.target.value
-        this.setState({battleshipLocation7: clonedBattleshipLocation7})
-      }
-      if (event.target.name === "battleshipLocation8") {
-        let clonedBattleshipLocation8 = this.state.battleshipLocation8
-        clonedBattleshipLocation8 = event.target.value
-        this.setState({battleshipLocation8: clonedBattleshipLocation8})
-      }
+        this.setState({[event.target.name]: event.target.value})
+        console.log(this.state)
   }
 
   handleSubmit(event) {
+    var locations = [this.state.dinghyLocation1, this.state.dinghyLocation2, this.state.dinghyLocation3, this.state.submarineLocation1, this.state.submarineLocation2, this.state.submarineLocation3, this.state.submarineLocation4, this.state.destroyerLocation1, this.state.destroyerLocation2, this.state.destroyerLocation3, this.state.destroyerLocation4, this.state.destroyerLocation5, this.state.destroyerLocation6, this.state.battleshipLocation1, this.state.battleshipLocation2, this.state.battleshipLocation3, this.state.battleshipLocation4, this.state.battleshipLocation5, this.state.battleshipLocation6, this.state.battleshipLocation7, this.state.battleshipLocation8]
     let clonedAllPlayerLocations = this.state.allPlayerLocations
-    clonedAllPlayerLocations[this.state["dinghyLocation1"]] = this.state.dinghyLocation1
-    clonedAllPlayerLocations[this.state["dinghyLocation2"]] = this.state.dinghyLocation2
-    clonedAllPlayerLocations[this.state["dinghyLocation3"]] = this.state.dinghyLocation3
-    clonedAllPlayerLocations[this.state["submarineLocation1"]] = this.state.submarineLocation1
-    clonedAllPlayerLocations[this.state["submarineLocation2"]] = this.state.submarineLocation2
-    clonedAllPlayerLocations[this.state["submarineLocation3"]] = this.state.submarineLocation3
-    clonedAllPlayerLocations[this.state["submarineLocation4"]] = this.state.submarineLocation4
-    clonedAllPlayerLocations[this.state["destroyerLocation1"]] = this.state.destroyerLocation1
-    clonedAllPlayerLocations[this.state["destroyerLocation2"]] = this.state.destroyerLocation2
-    clonedAllPlayerLocations[this.state["destroyerLocation3"]] = this.state.destroyerLocation3
-    clonedAllPlayerLocations[this.state["destroyerLocation4"]] = this.state.destroyerLocation4
-    clonedAllPlayerLocations[this.state["destroyerLocation5"]] = this.state.destroyerLocation5
-    clonedAllPlayerLocations[this.state["destroyerLocation6"]] = this.state.destroyerLocation6
-    clonedAllPlayerLocations[this.state["battleshipLocation1"]] = this.state.battleshipLocation1
-    clonedAllPlayerLocations[this.state["battleshipLocation2"]] = this.state.battleshipLocation2
-    clonedAllPlayerLocations[this.state["battleshipLocation3"]] = this.state.battleshipLocation3
-    clonedAllPlayerLocations[this.state["battleshipLocation4"]] = this.state.battleshipLocation4
-    clonedAllPlayerLocations[this.state["battleshipLocation5"]] = this.state.battleshipLocation5
-    clonedAllPlayerLocations[this.state["battleshipLocation6"]] = this.state.battleshipLocation6
-    clonedAllPlayerLocations[this.state["battleshipLocation7"]] = this.state.battleshipLocation7
-    clonedAllPlayerLocations[this.state["battleshipLocation8"]] = this.state.battleshipLocation8
+    var i;
+    for (i = 0; i < locations.length; i++) {
+      var location = locations[i]
+      clonedAllPlayerLocations[location] = "X";
+    }
     this.setState({allPlayerLocations: clonedAllPlayerLocations})
     event.preventDefault()
     this.setState({
@@ -186,7 +68,7 @@ export class BoatLocationForm extends React.Component {
         The Dinghy:
         <br></br><input type="text" name="dinghyLocation1" placeholder="Coordinate 1" onChange={(e) => this.handleChange(e)}></input>
         <br></br><input type="text" name="dinghyLocation2" placeholder="Coordinate 2" onChange={(e) => this.handleChange(e)}></input>
-        <br></br><input type="text" name="dinghyLocation3" placeholder="Coordinate 3" onChange={(e) => this.handleChange(e)}></input>
+        <br></br><input type="text" name="dinghyLocation3" placeholder="Coordinate 3" onChange={(e) => this.handleChange(e, "dinghyLocation3")}></input>
         <br></br>The Submarine:
         <br></br><input type="text" name="submarineLocation1" placeholder="Coordinate 1" onChange={(e) => this.handleChange(e)}></input>
         <br></br><input type="text" name="submarineLocation2" placeholder="Coordinate 2" onChange={(e) => this.handleChange(e)}></input>
